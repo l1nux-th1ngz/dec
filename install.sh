29e36319530fee839652dcac07c3eeea65c63cf5
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-12-13 16:33:24 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ chmod +x *.sh
 echo "Starting install scripts, the script will wait while each runs."
 
 # Run installation scripts in sequence, waiting 7 seconds between each
-for script in 1-xorg.sh 2-2-bspwm.sh 3-diskutils.sh 4-bluetooth.sh 5-utils.sh 6-polkit.sh; do
+for script in 1-xorg.sh 2-2-bspwm.sh 3-diskutils.sh 4-bluetooth.sh 5-utils.sh 6-polkit.sh 7-zsh.sh; do
     if [ -f "$script" ]; then
         echo "Running $script..."
         sh "$script"
